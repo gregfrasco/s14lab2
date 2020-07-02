609cc8218ee3cb1a45f4b3fcb33145f074e80ba0
Greg Frasco <frascog17@gmail.com>
2020-07-02 08:32:24 -0400

    feat: predictive website

--- app.py
+++ app.py
@@ -5,17 +5,19 @@ import joblib
 
 linear_regression = joblib.load('./notebooks/linear-regr.pkl')
 decision_tree = joblib.load('./notebooks/decision-tree.pkl')
-sgd = joblib.load('./notebooks/sgd.pkl')
 knn = joblib.load('./notebooks/knn.pkl')
 
 
 @app.route('/')
 def index():
-    return redirect('/predict/0/4/2.5/3005/15/17903.0')
+    return redirect('/predict/0/4/2.5/3005/15/17903.0/1')
 
-@app.route('/predict/<int:model>/<beds>/<baths>/<sqft>/<age>/<lotSize>')
-def predict(model, beds, baths, sqft, age, lotSize):
-    return render_template('index.html', model=str(model), beds=str(beds), baths=str(baths), sqft=str(sqft), age=str(age), lotSize=str(lotSize))
+@app.route('/predict/<int:model>/<beds>/<baths>/<sqft>/<age>/<lotSize>/<int:garage>')
+def predict(model, beds, baths, sqft, age, lotSize, garage):
+    m = getModel(model)
+    prediction = m.predict([[float(beds), float(baths), float(sqft), float(age), float(lotSize), garage]])[0][0].round(0)
+    prediction = "${:,.0f}".format(prediction)
+    return render_template('index.html', model=str(model), beds=str(beds), baths=str(baths), sqft=str(sqft), age=str(age), lotSize=str(lotSize), garage=str(garage), prediction=prediction)
 
 def getModel(num):
-    return [linear_regression,decision_tree,sgd,knn][num]
+    return [linear_regression,decision_tree,knn][num]
